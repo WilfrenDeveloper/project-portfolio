fae87e8abc6712744bc38ea6162763b179161283
WilfrenDeveloper <https://github.com/WilfrenDeveloper/e-commerce-whit-React.git>
2023-11-26 22:26:58 -0500

    advances

--- src/components/Main/ContactMe.jsx
+++ src/components/Main/ContactMe.jsx
@@ -4,20 +4,30 @@ const ContactMe = () => {
     return (
         <section className='contact__section' id="contact">
             <h2 className='contact__h2'>Contacta me...</h2>
-            <form action="https://formsubmit.co/wilfrenmoney@gmail.com" method="POST">
-                <label htmlFor="">Nombre</label>
-                <input type="text" />
-                <label htmlFor="">Email</label>
-                <input type="text" />
-                <label htmlFor="">Mensaje</label>
-                <textarea name="message" id="message" cols="30" rows="3" placeholder='Mensaje'></textarea>
-                <button>
-                    <span>Enviar </span>
-                    <i class='bx bx-right-arrow-alt'></i>
+            <form className='contact__form' action=''>
+                <label className='contact__label contact__name' htmlFor=""><span>Nombre</span>
+                    <input className='contact__input input__name' type="name" placeholder='Ingresa tu nombre' />
+                </label>
+
+                <label className='contact__label contact__email' htmlFor="">
+                    <span>Email</span>
+                    <input className='contact__input input__email' type="email" placeholder='Ingresa tu correo' />
+                </label>
+
+                <label className='contact__label contact__message' htmlFor="">
+                    <span>Mensaje</span>
+                    <textarea className='contact__input input__message' name="message" id="message" cols="30" rows="3" placeholder='Mensaje'></textarea>
+                </label>
+
+                <div>
+                <button className='contact__button' formTarget=''>
+                    <span className='contact__button--span'>Enviar </span>
+                    <i class='bx bx-right-arrow-alt bx-flip-vertical bx-flashing' ></i>
                 </button>
+                </div>
             </form>
         </section>
-  )
+    )
 }
 
 export default ContactMe
