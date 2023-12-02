5eb4f4a2f46c1809d757310ba40e4ce1a5ea4618
WilfrenDeveloper <https://github.com/WilfrenDeveloper/e-commerce-whit-React.git>
2023-12-02 16:35:24 -0500

    advances

--- src/components/Main/ContactMe.jsx
+++ src/components/Main/ContactMe.jsx
@@ -2,31 +2,47 @@ import '../../style/ContactMe.css'
 
 const ContactMe = () => {
     return (
-        <section className='contact__section' id="contact">
-            <h2 className='contact__h2'>Contacta me...</h2>
-            <form className='contact__form' action="https://formsubmit.co/wilfrenmoney@gmail.com" method="POST" >
-                <label className='contact__label contact__name' htmlFor=""><span>Nombre</span>
-                    <input className='contact__input input__name' type="name" name="name" placeholder='Ingresa tu nombre' />
-                </label>
+        <>
+            <section className='contact__section' id="contact">
+                <h2 className='contact__h2'>Contacta me...</h2>
+                <form className='contact__form' action="https://formsubmit.co/wilfrenmoney@gmail.com" method="POST" >
+                    <label className='contact__label contact__name' htmlFor=""><span>Nombre</span>
+                        <input className='contact__input input__name' type="name" name="name" placeholder='Ingresa tu nombre' />
+                    </label>
 
-                <label className='contact__label contact__email' htmlFor="">
-                    <span>Email</span>
-                    <input className='contact__input input__email' type="email" name="email" placeholder='Ingresa tu correo' />
-                </label>
+                    <label className='contact__label contact__email' htmlFor="">
+                        <span>Email</span>
+                        <input className='contact__input input__email' type="email" name="email" placeholder='Ingresa tu correo' />
+                    </label>
 
-                <label className='contact__label contact__message' htmlFor="">
-                    <span>Mensaje</span>
-                    <textarea className='contact__input input__message' name="message" id="message" cols="30" rows="3" placeholder='Mensaje'></textarea>
-                </label>
+                    <label className='contact__label contact__message' htmlFor="">
+                        <span>Mensaje</span>
+                        <textarea className='contact__input input__message' name="message" id="message" cols="30" rows="3" placeholder='Mensaje'></textarea>
+                    </label>
 
-                <div>
-                <button className='contact__button' formTarget=''>
-                    <span className='contact__button--span'>Enviar </span>
-                    <i className='bx bx-right-arrow-alt bx-flip-vertical bx-flashing' ></i>
-                </button>
-                </div>
-            </form>
-        </section>
+                    <div>
+                        <button className='contact__button' formTarget=''>
+                            <span className='contact__button--span'>Enviar </span>
+                            <i className='bx bx-right-arrow-alt bx-flip-vertical bx-flashing' ></i>
+                        </button>
+                    </div>
+                </form>
+            </section>
+            <footer className='contact__footer'>
+                <p>
+                    <i class='bx bx-copyright'></i>
+                    <span>Wilfren Quintero 2023</span>
+                </p>
+                <ul className='contact__ul'>
+                    <a href="https://github.com/WilfrenDeveloper" target='_blank'>
+                        <i class='bx bxl-linkedin-square' ></i>
+                    </a>
+                    <a href="https://www.linkedin.com/in/oscar-wilfren-quintero-ramos-5a8323294/" target='_blank'>
+                        <i class='bx bxl-github' ></i>
+                    </a>
+                </ul>
+            </footer>
+        </>
     )
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -15,15 +15,14 @@ const Nav = () => {
     <nav className='nav'>
       <button onClick={handleClick} className='nav__button'>
         <i className={`bx bx-minus ${(menu === true)?"bx-10":"bx-1"}`}></i>
-        <i className={`bx bx-minus ${(menu === true)?"bx-20":"bx-2"}`}></i>
         <i className={`bx bx-minus ${(menu === true)?"bx-30":"bx-3"}`}></i>
       </button>
       <div className={`nav__container ${(menu === true)?"nav__div":""}`}>
-        <a href="#root">Inicio</a>
-        <a href="#about">Sobre mí</a>
-        <a href="#skill">Habilidades técnicas</a>
-        <a href="#proyects">Mis proyectos</a>
-        <a href="#contact">Contacto</a>
+        <a className='nav__a' href="#root">Inicio</a>
+        <a className='nav__a' href="#about">Sobre mí</a>
+        <a className='nav__a' href="#skill">Habilidades técnicas</a>
+        <a className='nav__a' href="#proyects">Mis proyectos</a>
+        <a className='nav__a' href="#contact">Contacto</a>
       </div>
     </nav>
   )
